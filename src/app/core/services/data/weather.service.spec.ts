a0e18aa9e845cb74962a99e2c7ea944faeaa0faa
Daniel Fader <daniel@fader.info>
2021-09-25 23:51:49 +0200

    Weather service test cases

--- src/app/core/services/data/weather.service.spec.ts
+++ src/app/core/services/data/weather.service.spec.ts
@@ -13,4 +13,15 @@ describe('WeatherService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return the required amount of data entries', () => {
+    const length = 5;
+    expect(service.getRandomData(length)).toHaveSize(length);
+  });
+
+  it('should deliver wind direction', () => {
+    const length = 5;
+    const data = service.getRandomData(length);
+    expect(data.find(weatherDataEntry => !weatherDataEntry.direction)).toBeFalsy();
+  });
 });
